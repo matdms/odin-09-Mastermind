8f42c3ad0b05bb5f0bcb2f8ab27f9b189cd33485
matdms <mathieu.desmoulins@gmail.com>
2020-05-01 23:00:42 +0200

    Display the game

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mastermind
+### in Ruby, form the command line
+Build a Mastermind game from the command line where you have 12 turns to guess the secret code, starting with you guessing the computer’s random code.
---
+++ mastermind.rb
@@ -0,0 +1,128 @@
+# Classes
+
+# Board
+class Board
+  attr_accessor :code, :propositions, :reponses
+  
+  def initialize(nom_joueur, code, propositions, reponses)
+    @nom_joueur = nom_joueur
+    @code = code
+    @propositions = propositions
+    @reponses = reponses
+  end
+
+  def display()
+    system "clear"
+    puts "  **************"
+    puts "  * MASTERMIND *"
+    puts "  **************"
+    puts " "
+    puts "  Joueur: #{@nom_joueur}"
+    puts ""
+    puts "  CODE   X X X X"
+    for i in 0..11 do
+      j = 12-i-1
+      puts "    #{j+1<10 ? ' ' : ''}#{j+1}   #{@propositions[j][0]} #{@propositions[j][1]} #{@propositions[j][2]} #{@propositions[j][3]}   #{@reponses[j][0]}#{@reponses[j][1]}#{@reponses[j][2]}#{@reponses[j][3]}"
+    end
+    puts " "
+  end
+
+
+end
+
+
+# Player
+class Player
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+  
+
+end
+
+
+
+# FONCTIONS
+
+# check validité proposition joueur
+def check_valid(tentative)
+  a = 0 # reste à 0 tant que la tentative est valide
+  tentative.length != 4 ? a += 1 : a
+  tentative = tentative.split(//)
+
+
+  # si tout est ok, on renvoi un array de la tentative, sinon false
+  if a == 0
+    return tentative #.split(//)
+  else 
+    return false
+  end
+end
+
+
+# initialisation
+  # initialiser les variables
+  # array du code, des propositions, des reponses
+code = [0, 0, 0, 0]
+propositions = [  ["-", "-", "-", "-"],
+                  ["-", "-", "-", "-"],
+                  ["-", "-", "-", "-"],
+                  ["-", "-", "-", "-"],
+                  ["-", "-", "-", "-"],
+                  ["-", "-", "-", "-"],
+                  ["-", "-", "-", "-"],
+                  ["-", "-", "-", "-"],
+                  ["-", "-", "-", "-"],
+                  ["-", "-", "-", "-"],
+                  ["-", "-", "-", "-"],
+                  ["-", "-", "-", "-"], ]
+
+reponses = [  [".", ".", ".", "."],
+              [".", ".", ".", "."],
+              [".", ".", ".", "."],
+              [".", ".", ".", "."],
+              [".", ".", ".", "."],
+              [".", ".", ".", "."],
+              [".", ".", ".", "."],
+              [".", ".", ".", "."],
+              [".", ".", ".", "."],
+              [".", ".", ".", "."],
+              [".", ".", ".", "."],
+              [".", ".", ".", "."] ]
+
+  # creer un joueur
+joueur = Player.new("Mathieu")
+# nom_joueur = joueur.name
+
+  # generer le code
+code = [1, 2, 3, 4]
+
+  # creer une board
+partie = Board.new(joueur.name, code, propositions, reponses)
+
+  # lancer la boucle de la partie
+game = 1
+    # effacer / redessiner le jeu
+    # demander une proposition au joueur
+    # check validité de la proposition
+    # traiter la proposition (générer une réponse)
+    # loop
+
+tour = 0
+
+until game != 1 do
+  tour += 1
+  tour >= 12 ? game = 0 : game = 1
+  partie.display
+  puts "Nouvelle tentative ? (ex: 1234)"
+  tentative = gets.chomp
+  if check_valid(tentative)
+    partie.propositions[tour-1] = check_valid(tentative)
+    # generer la reponse
+  else 
+    partie.propositions[tour-1] = ["E", "R", "R", "."]
+  end
+end
+
+partie.display
